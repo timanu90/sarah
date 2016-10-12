5b0b8941d0b82f57f4e049fd1ccf8046d8ec8ba1
timanu90 <tiago.vasconcelos.18@gmail.com>
2016-10-12 10:26:41 +0100

    Added input treatment to uniform letters to upper case and remove special charateres

--- src/chatbot.cpp
+++ src/chatbot.cpp
@@ -25,6 +25,25 @@ std::string __remove_special_char(std::string str)
 	return ret;
 }
 
+std::string __convert_to_upper_case(std::string str)
+{
+	std::string ret = "";
+
+	for(int i=0; i<str.length(); i++)
+	{
+		if( (str[i] >= 'A' && str[i] <= 'Z') || str[i] == ' ')
+		{
+			ret += str[i];
+		}
+		else
+		{
+			ret += str[i] - 32;
+		}
+	}
+
+	return ret;
+}
+
 void TChatBot::botWork()
 {
 	std::string input 		= "";
@@ -37,6 +56,9 @@ void TChatBot::botWork()
 		getline(std::cin, input);
 
 		input = __remove_special_char(input);
+		input = __convert_to_upper_case(input);
+
+		std::cout<<"worked input = "<<input<<std::endl;
 
 		if(input != "")
 		{
@@ -49,13 +71,13 @@ void TChatBot::botWork()
 			}
 			else
 			{
-				if(input != "bye")
+				if(input != "BYE")
 				{
 					response = know.getResponse(input);
 				}
 			}
 		
-			if(input != "bye")
+			if(input != "BYE")
 			{
 				/* Regist bot response in log */
 				log.writeBotResponde(response);
@@ -69,6 +91,6 @@ void TChatBot::botWork()
 			std::cout<<"Say something"<<std::endl;
 		}
 
-	}while(input != "bye");
+	}while(input != "BYE");
 	
 }
